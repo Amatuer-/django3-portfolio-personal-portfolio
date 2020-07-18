ee53962eba51539baf42668c4affd1214f6f30e6
Ashish Dubey <Ashu.7dubey@gmail.com>
2020-07-18 16:03:33 +0530

    first commit

---
+++ portfolio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from portfolio import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('', views.home , name='home')
+]
+
---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from portfolio.templates import portfolio
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'portfolio/home.html', context )
+        #HttpResponse('images\Mypic1.jpg')
